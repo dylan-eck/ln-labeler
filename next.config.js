98febb92ea2dbdf9c14d0e7268415d69b715e5b5
Dylan Eck <akdylaneck@gmail.com>
2022-11-20 18:25:20 -0700

    remove unneeded configurations from next.config.js

--- next.config.js
+++ next.config.js
@@ -2,16 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-        port: "",
-        pathname: "/*",
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
